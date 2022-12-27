a979b82e689d7661273b2c19e0c60e339704a309
Direwolf23 <bhupendraprajapati936@gmail.com>
2022-12-27 15:59:49 +0530

    Done

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,15 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals'; // REACT DEPENDENCIES
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals'; // REACT DEPENDENCIES
+import { AppProvider } from '@shopify/polaris';
+import en from '@shopify/polaris/locales/en.json';
+import '@shopify/polaris/build/esm/styles.css';
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from './reportWebVitals';
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppProvider i18n={en}>
+        <App />
+    </AppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
